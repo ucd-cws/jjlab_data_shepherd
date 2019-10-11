1cd85be3b207c2f106c60244cddb1be743c6c7c9
Ryan Peek <rapeek@ucdavis.edu>
2019-10-11 14:18:24 -0700

    update accessing access

--- code/access_mdb.R
+++ code/access_mdb.R
@@ -7,28 +7,53 @@ library(janitor)
 
 
 # set path to .mdb
-mdblink <- as_fs_path("~/Downloads/JJLAB_DB_v1.1_backend_mdb.mdb")
+mdblink <- as_fs_path("~/Downloads/JJLAB_DB_v1.1_backend_mdb.mdb") # local version
 
-#mdblink <- as_fs_path("/Volumes/rapeek/public/eFlows/California_Hydro_Geomorphic_Classification.mdb")
+mdblink <- as_fs_path("/Volumes/projects3/jj_lab/database/JJLAB_DB_v1.1backend_mdb.mdb") # through VPN
+
 
 # see table names:
 mdb.get(mdblink, tables=TRUE)
 
 # get single table
-samples <- mdb.get(mdblink, tables="samples") %>% 
-  # clean names w janitor
+coll_info <- mdb.get(mdblink, tables="collection_info", stringsAsFactors=F) 
+
+# pull the data in and clean the column names at the same time
+coll_info_clean <- mdb.get(mdblink, tables="collection_info") %>% 
+  # clean column names w janitor (makes all spaces/uppercase/periods into lower case and underscores)
   clean_names()
 
-# view fields
-names(samples)
 
-# Bonus: Make it Spatial --------------------------------------------------
+# Make the Datetime R Friendly --------------------------------------------
+
+library(lubridate)
+
+# one way
+coll_info$collect.date <- as.Date(mdy_hms(coll_info$collect.date))
+
+summary(coll_info$collect.date)
+
+# get rid of standard date in the collect.time 
+coll_info <- coll_info %>% tidyr::separate(collect.time, into=c("date_old", "colltime24"), " ")
+
+# make a final datetime col
+coll_info$collect.datetime <- ymd_hms(paste0(coll_info$collect.date, " ", coll_info$colltime24))
+
+
+
+# Make a Map of Samples ---------------------------------------------------
+
+sites <- mdb.get(mdblink, tables="sites", stringsAsFactors=F) 
 
 library(sf)
 library(mapview)
+
 # make data into sf layer
-ref_gages_sf <- st_as_sf(ref_gages, coords = c("longdd","latdd"), remove = F, crs=4326)
-names(ref_gages_sf)
+sites_sf <- sites %>% 
+  filter(!is.na(site.long)) %>%  # need to remove all NAs 
+  st_as_sf(coords = c("site.long","site.lat"), remove = F, crs=4326)
+
+names(sites_sf)
 
 # view
-mapview(ref_gages_sf)
+mapview(sites_sf)
